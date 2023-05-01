d04bc3fb6e87ae9c5093c03dc44798512b074aa3
Reagan Odhiambo <reaganodhiambo003@gmail.com>
2023-05-02 01:26:36 +0300

    updates

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,49 +1,57 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # All the user accounts inherit from UserAccount model
-class UserAccount(models.Model):
+
+
+class UserAccount(AbstractUser):
     ACCOUNT_TYPES = [
         ('Manager', 'Manager'),
         ('Landlord', 'Landlord'),
         ('Tenant', 'Tenant'),
     ]
-    account_type = models.CharField(
-        max_length=250, choices=ACCOUNT_TYPES, default='Tenant')
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    email = models.EmailField(max_length=254, unique=True)
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    password = models.CharField(max_length=250)
+
+    # remove username as default for auth
+    username = None
+    # set email as unique identifier for auth
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     class Meta:
         abstract = True
 
 # Tenants Model
+
+
 class Tenant(UserAccount):
     budget = models.PositiveIntegerField()
     prefered_location = models.CharField(max_length=250)
     dp = models.ImageField(upload_to='Images/Dp/')
 
-
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
 
 # Landlords Model
+
+
 class Landlord(UserAccount):
     dp = models.ImageField(upload_to='Images/Dp/')
     company_name = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
 
 # Managers Model
+
+
 class PropertyManager(UserAccount):
     license_number = models.CharField(max_length=200)
     experience = models.IntegerField()
     services = models.CharField(max_length=255)
     dp = models.ImageField(upload_to='Images/Dp/')
 
-
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
