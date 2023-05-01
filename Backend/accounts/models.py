3f932e2d93bf965811fe02117e9a1555fd128226
Reagan Odhiambo <reaganodhiambo003@gmail.com>
2023-05-01 23:30:40 +0300

    Updated Models

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import User
 
+# All the user accounts inherit from UserAccount model
 class UserAccount(models.Model):
     ACCOUNT_TYPES = [
         ('Manager', 'Manager'),
         ('Landlord', 'Landlord'),
         ('Tenant', 'Tenant'),
     ]
-
-    account_type = models.CharField(max_length=250, choices=ACCOUNT_TYPES, default=Tenant)
+    account_type = models.CharField(
+        max_length=250, choices=ACCOUNT_TYPES, default='Tenant')
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     email = models.EmailField(max_length=254, unique=True)
@@ -17,10 +18,32 @@ class UserAccount(models.Model):
 
     class Meta:
         abstract = True
-    
 
+# Tenants Model
+class Tenant(UserAccount):
+    budget = models.PositiveIntegerField()
+    prefered_location = models.CharField(max_length=250)
+    dp = models.ImageField(upload_to='Images/Dp/')
+
+
+    def __str__(self):
+        return self.first_name
+
+# Landlords Model
+class Landlord(UserAccount):
+    dp = models.ImageField(upload_to='Images/Dp/')
+    company_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.first_name
+
+# Managers Model
 class PropertyManager(UserAccount):
+    license_number = models.CharField(max_length=200)
     experience = models.IntegerField()
     services = models.CharField(max_length=255)
+    dp = models.ImageField(upload_to='Images/Dp/')
 
 
+    def __str__(self):
+        return self.first_name
--- Backend/listings/models.py
+++ Backend/listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# class Owners(User):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# class Buyers(User):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# class Manager(User):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
